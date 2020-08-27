115fca3ae84054e79b7e5bbfbc470d84197995ff
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-26 22:05:24 -0700

    push

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+USE burger_db;
+
+INSERT INTO burgers (name, quantity, cost, purchased)
+VALUES ('Patty melt', 1, 3.00, false),
+  ('Cheeseburger', 5, 1.50, false),
+  ('Slopper', 1, 3, true),
+  ('Chili burger', 5, 3.25, true),
+  ('Salmon burger', 2, 4.50, false),
+  ('Pizza', 1, 1.25, false),
+  ('Luther Burger', 1, 2.50, true),
+  ('Slider', 1, 2.50, true),
+  ('Slugburger', 1, 4, false),
+  ('Jucy Lucy', 1, 3.5, false);
